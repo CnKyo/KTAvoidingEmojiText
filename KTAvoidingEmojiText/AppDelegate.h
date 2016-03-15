be264f991c2221edf7d773701fe4e1035cbcb56f
Kevin Tu <819931323@qq.com>
2016-03-15 19:18:39 +0800

    Initial Commit

---
+++ KTAvoidingEmojiText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTAvoidingEmojiText
+//
+//  Created by tujinqiu on 16/3/15.
+//  Copyright © 2016年 tujinqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
